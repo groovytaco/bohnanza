e16126a2a93fc348c0160d3ba8f0da7486d2aef2
wheatthin <essanchez02@gmail.com>
2022-03-04 22:55:07 -0700

    started card function

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -3,18 +3,11 @@ import Card from '../classes/Card.js';
 import "./Game.css";
 function Game ()
 {
-    var cards = [<Card type = "coffee" />,<Card type = "blue" />]
     return (
         <div className="Game">
-            <header className="Game-header">
-                <p>
-                    <font size="32">
-                        <b>Game</b>
-                    </font>
-                </p>
-            </header>
-        </div>,
-        cards.map((Card) => <div>{Card}</div>)
+        <Card type="chili" x="800px" y="700px"/>
+        <Card type = "blue" x="940px" y="700px"/>
+        </div>    
     );
 }
 
--- src/classes/Card.js
+++ src/classes/Card.js
@@ -1,31 +1,73 @@
-import React, { Component } from 'react';
-import coffee from '../coffee.png';
-import blue from '../blue.png';
+import React from 'react';
+import cocoa from '../Beans/Cocoa.jpg';
+import garden from '../Beans/Garden.jpg';
+import red from '../Beans/Red.jpg';
+import blackEyed from '../Beans/Black-eyed.jpg';
+import soy from '../Beans/Soy.jpg';
+import green from '../Beans/Green.jpg';
+import stink from '../Beans/Stink.jpg';
+import chili from '../Beans/Chili.jpg';
+import blue from '../Beans/Blue.jpg';
+import wax from '../Beans/Wax.jpg';
+import coffee from '../Beans/Coffee.jpg';
 import "./Card.css";
 
-class  Card extends Component {
-    constructor(props) {
-        super(props);
-        this.state =
-        {
-            type: ""
-        }
+function Card(props) {
+    var image;
+    var highlighted = false;
+    var xPos = props.x;
+    var yPos = props.y;
+    switch (props.type) {
+        case "cocoa":
+            image = cocoa;
+            break;
+        case "garden":
+            image = garden;
+            break;
+        case "red":
+            image = red;
+            break;
+        case "blackEyed":
+            image = blackEyed;
+            break;
+        case "soy":
+            image = soy;
+            break;
+        case "green":
+            image = green;
+            break;
+        case "stink":
+            image = stink;
+            break;
+        case "chili":
+            image = chili;
+            break;
+        case "blue":
+            image = blue;
+            break;
+        case "wax":
+            image = wax;
+            break;
+        case "coffee":
+            image = coffee;
+            break;
+        default:
     }
 
-    render() { 
-        if(this.props.type==="coffee")
-        {
-            this.image = coffee;
-        }else{
-            this.image = blue;
-        } 
-        return (  
-            <div className='Card'>
-                <h1> {this.props.type}</h1>
-                <img className = "bean-image" src={this.image} className="card-image" alt="" />
-            </div>
-        );
-    }
+    return (
+        <div className='Card' style={{left: xPos, top: yPos,}}>
+            <img className="bean-image"
+                src={image}
+                alt=""
+                style={{
+                    borderStyle: "solid",
+                    borderColor: "#fde32c",
+                    borderWidth: "5px",
+                    borderRadius: "5px"
+                }}>
+            </img>
+        </div>
+    );
 }
- 
+
 export default Card;
