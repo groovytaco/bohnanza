e2f64199eaab314761043d5b412f4b41f1bc5a09
wheatthin <essanchez02@gmail.com>
2022-03-06 19:31:17 -0700

    Worked on Deck class

--- src/Pages/ActiveGame.js
+++ src/Pages/ActiveGame.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react';
 import BeanField from '../classes/BeanField.js';
 import Deck from '../classes/Deck.js';
 import InfoCard from '../classes/InfoCard.js';
@@ -7,16 +8,29 @@ import PlayerHand from '../classes/PlayerHand.js';
 import "./ActiveGame.css";
 
 function ActiveGame() {
+    const [gameStatus, setGameStatus] = useState('PlantSecondOrFlip2');
+    const changeGameStatus= (data) =>
+    {
+        setGameStatus(data);
+    }
+    function nextTurn()
+    {
+        if(gameStatus=== "Flipped2Cards")
+        {
+            setGameStatus("WaitingForTurn");
+        }
+    }
   return (
       <div className="Active-game">
             <InfoCard/>
             <InfoCard/>
             <InfoCard/>
             <InfoCard/>
-            <Deck/>
+            <Deck changeGameStatus = {changeGameStatus} gameStatus = {gameStatus}/>
             <TradeTable/>
             <BeanField/>
             <PlayerHand/>
+            <h1>{gameStatus}</h1>
             <button className="Next-turn"
                 title="NextTurn"
                 style={{
@@ -26,6 +40,7 @@ function ActiveGame() {
                     width: "4vw",
                     height: "4vw"
                 }}
+                onClick={nextTurn}
             ></button>
         </div>
   );
--- src/classes/Deck.js
+++ src/classes/Deck.js
@@ -1,10 +1,33 @@
 import "./Deck.css";
 import DeckImage from "../Deck.jpg";
+import Card from "./Card";
 
-function Deck() {
+function Deck(props) {
+    var coffeeNum = 24;
+    var waxNum = 22;
+    var blueNum = 20;
+    var chiliNum = 18;
+    var stinkNum = 16;
+    var greenNum = 14;
+    var soyNum = 12;
+    var blackEyedNum = 10;
+    var redNum = 8;
+    var gardenNum = 6;
+    var cocoaNum = 4;
+    var leftBean = "coffee";
+    var rightBean = "chili";
+    function deckClicked()
+    {
+        if(props.gameStatus === "PlantSecondOrFlip2")
+        {
+            props.changeGameStatus("Flipped2Cards");
+        }
+    }
     return (
-        <div className="Deck">
+        <div className="Deck" onClick={deckClicked}>
             <img className="DeckImage" src={DeckImage} alt="DeckImage" />
+            <Card type = {leftBean} x="15vw" y="1vw"/>
+            <Card type = {rightBean} x="25vw" y="1vw"/>
         </div>
     );
 }
